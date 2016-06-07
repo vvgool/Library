8bd5f8287f179bc88f522069d9330a26262d7e21
Wiesen Wang <vv_gool@163.com>
2016-06-07 18:20:24 +0800

    add payLib

---
+++ PayLib/src/main/java/com/vv/paylib/PayActivity.java
@@ -0,0 +1,8 @@
+package com.vv.paylib;
+
+import vv.codelib.NormalBase.NormalBaseActivity;
+
+public abstract class PayActivity extends NormalBaseActivity {
+
+
+}
---
+++ PayLib/src/main/java/com/vv/paylib/TestActivity.java
@@ -0,0 +1,18 @@
+package com.vv.paylib;
+
+import android.os.Bundle;
+
+/**
+ * Created by vvgool on 16-6-7.
+ */
+public class TestActivity extends PayActivity {
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
